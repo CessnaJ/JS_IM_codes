7669ea32a47ccc63a3fd65fd0c0b964ea7f67c58
CessnaJ <eon8748@gmail.com>
2023-01-16 21:31:48 +0900

    farmcomeus0116

---
+++ FarmComeusfrontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Postcode from "./components/postcode/postcodeInput";
+
+function App() {
+  return (
+    <div className="App">
+      <Postcode />
+    </div>
+  );
+}
+
+export default App;
---
+++ FarmComeusfrontend/src/components/login/kakaoLogin.js
@@ -0,0 +1,7 @@
+function KakaoLogin(props) {
+  jsApiKey = "c22e57c71912fb59eda529f5a58ef168";
+  restApiKey = "1da7b3747e086ab039713b8c280ee6d5";
+  // 보안은 기본틀 만들고나서부터.. 어차피 private repo에 올릴것임
+}
+
+export default KakaoLogin;
